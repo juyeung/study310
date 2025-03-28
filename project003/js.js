83e1cd98242bd0b97a4b91c3893fcd1a45548322
zzuyeong <jijumto04@gmail.com>
2025-03-28 18:21:47 +0900

    20250328

--- project003/js.js
+++ project003/js.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // 화면의 높이값을 구해라
     let ht = $(window).height()
     console.log(ht);
-    $('#wrap>div').eq(2).height(ht+300)
+    // $('#wrap>div').eq(2).height(ht+300)
 
     // nav li 에 마우스가 들어갔을때, 나 자신에게 addClass를 해라
     $('nav li').click(function(){
@@ -20,6 +20,86 @@ $(document).ready(function(){
         console.log('박스'+i+'의 거리는'+divot);
 
         $('html,body').stop().animate({'scrollTop':divot},800)
+
+        // 두 번째 li 클릭시 nav의 색을 #fff로 변경
+        if ($(this).index() === 1) {
+            $("nav p").css("color", "#fff");
+            $("nav .dot").css("background-color", "#fff");  // 두 번째 li 클릭 시
+        } else {
+            $("nav p").css("color", "");
+            $("nav .dot").css("background-color", "");  // 두 번째 li가 아니면 원래 색으로
+        }
+   
     });
+
+    // player 네비게이션
+    $('.box2 .navi p').click(function() {
+        let index = $(this).index(); // 클릭한 p의 인덱스 (0부터 시작)
+
+        $('.box2>div:not(.navi)').removeClass('on'); // 모든 div에서 active 제거
+        $('.box2>div').eq(index).addClass('on'); // 해당 인덱스의 div에 active 추가
+        
+        $('.box2 .navi p').removeClass('on')
+        $(this).addClass('on')
+    });
+
+    $('.box3 .navi p').click(function() {
+        let index = $(this).index(); // 클릭한 p의 인덱스 (0부터 시작)
+
+        $('.box3 .player').removeClass('on'); // 모든 div에서 active 제거
+        $('.box3 .player').eq(index).addClass('on'); // 해당 인덱스의 div에 active 추가
+        
+        $('.box3 .navi p').removeClass('on')
+        $(this).addClass('on')
+    });
+
+
+    // 갤러리
+    $('article').mouseenter(function(){
+        $(this).parent('.panolama').addClass('on')
+    })
+
+    $('article').mouseleave(function(){
+        $(this).parent('.panolama').removeClass('on')
+    })
+
+    // article 클릭 시 팝업 열기
+    const $modal = $(".modal");
+    const $modalImg = $(".modal img");
+    const $closeBtn = $(".modal .close");
+
+    $(".box3 .panolama article").click(function () {
+        let imgSrc = $(this).find("img").attr("src");
+    
+        console.log("수정된 이미지 경로:", imgSrc);
+    
+        if (imgSrc) {
+            $(".modal img").attr("src", imgSrc);
+            $(".modal").css("display", "flex").hide().fadeIn();
+        }
+        $("body").css("overflow", "hidden");
+    });
+
+    // 닫기 버튼 클릭 시 팝업 닫기
+    $closeBtn.click(function () {
+        $modal.fadeOut();
+    });
+
+    // 모달 배경 클릭 시 닫기
+    $modal.click(function (e) {
+        if ($(e.target).is($modal)) {
+            $modal.fadeOut();
+        }
+        $("body").css("overflow", "auto");
+    });
+
+    // ESC 키 누르면 닫기
+    $(document).keydown(function (e) {
+        if (e.key === "Escape") {
+            $modal.fadeOut();
+        }
+        $("body").css("overflow", "auto");
+    });
+    
 
 })
