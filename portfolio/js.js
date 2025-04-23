5210a17985452f7170ad10da62a7864f1504d170
juyeung <jijumto04@gmail.com>
2025-04-23 17:41:20 +0900

    20250423

--- portfolio/js.js
+++ portfolio/js.js
@@ -34,7 +34,28 @@ $(document).ready(function(){
     $('article').eq(0).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
-        $('section').eq(0).addClass('on')
+        $('.wrap .project01').addClass('on')
     })
+    $('article').eq(1).click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project02').addClass('on')
+    })
+    $('article').eq(2).click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project03').addClass('on')
+    })
+
+    // 깃허브페이지 연결
+    $('.project01 .gitadd').on('click', function() {
+        window.open('https://juyeung.github.io/study310/project001/index.html', '_blank');
+    });
+    $('.project02 .gitadd').on('click', function() {
+        window.open('https://juyeung.github.io/study310/project002/index.html', '_blank');
+    });
+    $('.project03 .gitadd').on('click', function() {
+        window.open('https://juyeung.github.io/study310/project003/index.html', '_blank');
+    });
 
 }) // 스크립트 끝
