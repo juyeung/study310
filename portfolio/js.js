cd0104749e5f48d53c00365fb5b1f923e83aee47
juyeung <jijumto04@gmail.com>
2025-04-30 17:43:15 +0900

    20250430

--- portfolio/js.js
+++ portfolio/js.js
@@ -9,19 +9,25 @@ $(document).ready(function(){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap>div').eq(0).addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#FF7A1A')
+        $('header .box1').css('background-color','#FF7A1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
     $('nav p').eq(1).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap>div').eq(1).addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#FF7A1A')
+        $('header .box1').css('background-color','#FF7A1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
     $('nav p').eq(2).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap>div').eq(2).addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#FF7A1A')
+        $('header .box1').css('background-color','#FF7A1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
 
     // logo를 클릭했을때, main에 on이 붙어라
@@ -31,7 +37,9 @@ $(document).ready(function(){
         $('.wrap>div').eq(0).addClass('on')
         $('nav p').removeClass('on')
         $('nav p').eq(0).addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#FF7A1A')
+        $('header .box1').css('background-color','#FF7A1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
 
     // 프로젝트 상세페이지 연결d
@@ -39,37 +47,82 @@ $(document).ready(function(){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project01').addClass('on')
-        $('.header02').css('background-color','#F9FBFF')
+        $('body').css('background-color','#3787FF')
+        $('header .box1').css('background-color','#3787FF')
+        $('header .box2').css('background-color','#F9FBFF')
+        console.log('article')
     })
     $('article').eq(1).children('.mockup').click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project02').addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#C10C1A')
+        $('header .box1').css('background-color','#C10C1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
     $('article').eq(2).children('.mockup').click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project03').addClass('on')
-        $('.header02').css('background-color','#FBFBFA')
+        $('body').css('background-color','#66655D')
+        $('header .box1').css('background-color','#66655D')
+        $('header .box2').css('background-color','#FBFBFA')
     })
     $('.latestBox>div').eq(0).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project01').addClass('on')
-        $('.header02').css('background-color','#F9FBFF')
+        $('body').css('background-color','#3787FF')
+        $('header .box1').css('background-color','#3787FF')
+        $('header .box2').css('background-color','#F9FBFF')
     })
     $('.latestBox>div').eq(1).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project02').addClass('on')
-        $('.header02').css('background-color','#FFFAEE')
+        $('body').css('background-color','#C10C1A')
+        $('header .box1').css('background-color','#C10C1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
     $('.latestBox>div').eq(2).click(function(e){
         e.preventDefault()
         $('.wrap>div').removeClass('on')
         $('.wrap .project03').addClass('on')
-        $('.header02').css('background-color','#FBFBFA')
+        $('body').css('background-color','#66655D')
+        $('header .box1').css('background-color','#66655D')
+        $('header .box2').css('background-color','#FBFBFA')
+    })
+    $('.project01 .right').click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project02').addClass('on')
+        $('body').css('background-color','#C10C1A')
+        $('header .box1').css('background-color','#C10C1A')
+        $('header .box2').css('background-color','#FFFAEE')
+    })
+    $('.project02 .left').click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project01').addClass('on')
+        $('body').css('background-color','#3787FF')
+        $('header .box1').css('background-color','#3787FF')
+        $('header .box2').css('background-color','#F9FBFF')
+    })
+    $('.project02 .right').click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project03').addClass('on')
+        $('body').css('background-color','#66655D')
+        $('header .box1').css('background-color','#66655D')
+        $('header .box2').css('background-color','#FBFBFA')
+    })
+    $('.project03 .left').click(function(e){
+        e.preventDefault()
+        $('.wrap>div').removeClass('on')
+        $('.wrap .project02').addClass('on')
+        $('body').css('background-color','#C10C1A')
+        $('header .box1').css('background-color','#C10C1A')
+        $('header .box2').css('background-color','#FFFAEE')
     })
 
     // 깃허브페이지 연결
@@ -96,6 +149,26 @@ $(document).ready(function(){
         // 오른쪽 박스: 마우스가 오른쪽으로 갈수록 더 많이 보이게 (오른쪽으로 이동)
         $rightBox.css('clip-path', 'inset(0 0 0 ' + (100 - percentage) + '%)');
     });
+
+    // top 클릭시 페이지 상단으로 이동 스크롤위치값이 0일 때 top 숨김
+    // 초기 상태에서 숨김 처리
+    if ($(window).scrollTop() === 0) {
+        $('.top').hide();
+      }
+  
+      // 스크롤 시 .top 표시/숨김 처리
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() === 0) {
+          $('.top').fadeOut();
+        } else {
+          $('.top').fadeIn();
+        }
+      });
+  
+      // .top 클릭 시 맨 위로 스크롤
+      $('.top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+      });
 
    
 }) // 스크립트 끝
